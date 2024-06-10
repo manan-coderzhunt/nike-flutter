68849f1e7c0cde6b8f12f8634070f15784de2bb5
manan-coderzhunt <abdul.manan@coderzhunt.com>
2024-06-10 10:42:33 +0500

    new home screen

--- lib/Pages/home_screen.dart
+++ lib/Pages/home_screen.dart
@@ -9,7 +9,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   void _openLeftDrawer() {
@@ -29,12 +28,13 @@ class _HomeScreenState extends State<HomeScreen> {
           child: ListView(
             children: <Widget>[
               DrawerHeader(
-                child: Text('Left Drawer ',
-                style: TextStyle(
-                  color: Colors.white,
-                ),),
-                decoration: BoxDecoration(
+                child: Text(
+                  'Left Drawer ',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
+                decoration: BoxDecoration(),
               ),
               ListTile(
                 title: Text('Item 1'),
@@ -64,102 +64,111 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         backgroundColor: Color.fromRGBO(26, 37, 48, 1),
-        body:
-        ListView(
-            children: [
+        body: ListView(children: [
           Container(
-                  child: Stack(children: [
+              child: Stack(children: [
             Container(
-              margin: EdgeInsets.only( left: 15.0, right: 15.0),
+              margin: EdgeInsets.only(left: 15.0, right: 15.0),
               child: Column(children: [
                 SizedBox(height: 50.0),
                 Container(
-                child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child: Column(
                     children: [
-                    GestureDetector(
-                      onTap: _openLeftDrawer,
-                      child: SvgPicture.asset(
-                        "assets/images/2.svg", // Path of your icon
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Text('Store Location',
-                          style: TextStyle(
-                          color: Color.fromRGBO(112, 123, 129, 1),
-                          fontWeight: FontWeight.w100,
-                          fontFamily: 'Poppins1',
-                          fontSize: 14,
-                        ),
-                        ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          SvgPicture.asset(
-                            "assets/images/location.svg", // Path of your icon
+                          GestureDetector(
+                            onTap: _openLeftDrawer,
+                            child: SvgPicture.asset(
+                              "assets/images/2.svg", // Path of your icon
+                            ),
                           ),
-                          SizedBox(width: 5),
-                          Text('Mondolibug, Sylhet',style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: 'Poppins1',
-                            fontSize: 16,
+                          Column(
+                            children: [
+                              Text(
+                                'Store Location',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(112, 123, 129, 1),
+                                  fontWeight: FontWeight.w100,
+                                  fontFamily: 'Poppins1',
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  SvgPicture.asset(
+                                    "assets/images/location.svg", // Path of your icon
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    'Mondolibug, Sylhet',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: 'Poppins1',
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
+                          GestureDetector(
+                            onTap: _openLeftDrawer,
+                            child: SvgPicture.asset(
+                              "assets/images/3.svg", // Path of your icon
+                            ),
                           ),
                         ],
                       ),
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: _openLeftDrawer,
-                      child: SvgPicture.asset(
-                        "assets/images/3.svg", // Path of your icon
-                      ),
-                    ),
-                  ],),
-                ],),
+                    ],
+                  ),
                 ),
                 SizedBox(height: 30),
                 Row(
                   children: [
-                  SvgPicture.asset(
-                    width: 40,
-                    height: 45,
-                    "assets/images/Button.svg", // Path of your icon
-                  ),
-                ],),
+                    SvgPicture.asset(
+                      width: 40,
+                      height: 45,
+                      "assets/images/Button.svg", // Path of your icon
+                    ),
+                  ],
+                ),
                 SizedBox(height: 30),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                  SvgPicture.asset(
-                    "assets/images/Frame 7.svg", // Path of your icon
-                  ),
+                    SvgPicture.asset(
+                      "assets/images/Frame 7.svg", // Path of your icon
+                    ),
                     SvgPicture.asset(
                       "assets/images/Frame 8.svg", // Path of your icon
                     ),
-                  SvgPicture.asset(
-                    "assets/images/Frame 9.svg", // Path of your icon
-                  ),
-                  SvgPicture.asset(
-                    "assets/images/Frame 10.svg", // Path of your icon
-                  ),
-                  SvgPicture.asset(
-                    "assets/images/Frame 11.svg", // Path of your icon
-                  ),
-                ],),
+                    SvgPicture.asset(
+                      "assets/images/Frame 9.svg", // Path of your icon
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/Frame 10.svg", // Path of your icon
+                    ),
+                    SvgPicture.asset(
+                      "assets/images/Frame 11.svg", // Path of your icon
+                    ),
+                  ],
+                ),
                 SizedBox(height: 30),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Popular Shoes',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),),
-                    Text('See all',
+                    Text(
+                      'Popular Shoes',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'See all',
                       style: TextStyle(
                         color: Color.fromRGBO(91, 158, 225, 1),
                         fontSize: 16,
@@ -172,21 +181,24 @@ class _HomeScreenState extends State<HomeScreen> {
                 Row(
                   children: [
                     Container(
-                      padding: EdgeInsets.only(right: 5,left: 5),
+                      padding: EdgeInsets.only(right: 5, left: 5),
                       decoration: BoxDecoration(
                         color: Color.fromRGBO(22, 31, 40, 1),
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: Column(
                         children: [
-                          Image.asset('assets/images/splash1.png',
+                          Image.asset(
+                            'assets/images/splash1.png',
                             width: 150,
-                            height: 110,),
+                            height: 110,
+                          ),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(height: 20),
-                              Text('BEST SELLER',
+                              Text(
+                                'BEST SELLER',
                                 style: TextStyle(
                                   color: Color.fromRGBO(91, 158, 225, 1),
                                   fontSize: 14,
@@ -194,11 +206,13 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                               SizedBox(height: 5),
-                              Text('Nike Air Jordan',style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
+                              Text(
+                                'Nike Air Jordan',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
                             ],
                           ),
@@ -206,22 +220,25 @@ class _HomeScreenState extends State<HomeScreen> {
                           Row(
                             children: [
                               SizedBox(width: 15),
-                              Text('\$493.00',
+                              Text(
+                                '\$493.00',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
-                                ),),
+                                ),
+                              ),
                               SizedBox(width: 45),
                               SvgPicture.asset(
                                 "assets/images/Group 107.svg", // Path of your icon
                               ),
-                            ],)
+                            ],
+                          )
                         ],
                       ),
                     ),
                     SizedBox(width: 10),
                     Container(
-                      padding: EdgeInsets.only(right: 5,left: 5),
+                      padding: EdgeInsets.only(right: 5, left: 5),
                       decoration: BoxDecoration(
                         color: Color.fromRGBO(22, 31, 40, 1),
                         borderRadius: BorderRadius.circular(20),
@@ -229,14 +246,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Column(
                         children: [
                           SizedBox(height: 5),
-                          Image.asset('assets/images/splash3.png',
+                          Image.asset(
+                            'assets/images/splash3.png',
                             width: 150,
-                            height: 110,),
+                            height: 110,
+                          ),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(height: 20),
-                              Text('BEST SELLER',
+                              Text(
+                                'BEST SELLER',
                                 style: TextStyle(
                                   color: Color.fromRGBO(91, 158, 225, 1),
                                   fontSize: 14,
@@ -244,11 +264,13 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                               SizedBox(height: 5),
-                              Text('Nike Air Jordan',style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
+                              Text(
+                                'Nike Air Jordan',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
                             ],
                           ),
@@ -256,33 +278,38 @@ class _HomeScreenState extends State<HomeScreen> {
                           Row(
                             children: [
                               SizedBox(width: 15),
-                              Text('\$493.00',
+                              Text(
+                                '\$493.00',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
-                                ),),
+                                ),
+                              ),
                               SizedBox(width: 45),
                               SvgPicture.asset(
                                 "assets/images/Group 107.svg", // Path of your icon
                               ),
-                            ],)
+                            ],
+                          )
                         ],
                       ),
                     ),
-
                   ],
                 ),
                 SizedBox(height: 30),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('New Arrivals',
+                    Text(
+                      'New Arrivals',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16,
                         fontWeight: FontWeight.w600,
-                      ),),
-                    Text('See all',
+                      ),
+                    ),
+                    Text(
+                      'See all',
                       style: TextStyle(
                         color: Color.fromRGBO(91, 158, 225, 1),
                         fontSize: 16,
@@ -306,7 +333,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           SizedBox(height: 20),
-                          Text('Best Choice',
+                          Text(
+                            'Best Choice',
                             style: TextStyle(
                               color: Color.fromRGBO(91, 158, 225, 1),
                               fontSize: 16,
@@ -314,33 +342,38 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ),
                           SizedBox(height: 5),
-                          Text('Nike Air Jordan',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
+                          Text(
+                            'Nike Air Jordan',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                           SizedBox(height: 15),
-                          Text('\$849.69',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
+                          Text(
+                            '\$849.69',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ],
                       ),
-                      SizedBox(width:10),
+                      SizedBox(width: 10),
                       Column(
                         children: [
                           SizedBox(height: 20),
-                          Image.asset('assets/images/splash3.png',
+                          Image.asset(
+                            'assets/images/splash3.png',
                             width: 150,
-                            height: 110,),
+                            height: 110,
+                          ),
                         ],
                       )
                     ],
                   ),
-
                 ),
                 SizedBox(height: 30),
               ]),
@@ -349,22 +382,23 @@ class _HomeScreenState extends State<HomeScreen> {
         ]));
   }
 }
+
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(child: Text('Drawer Header'),
-              decoration: BoxDecoration(color: Colors.blue),
-            ),
-            ListTile(
-              title: Text('Item1'),
-            )
-          ],
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Drawer Header'),
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+            title: Text('Item1'),
+          )
+        ],
       ),
     );
   }
 }
-
